bc5801891e3d352b3d0cf5f547764920f92c068c
Cong Dinh <congdinhvannguyen165@outlook.com>
2021-06-25 17:48:03 +0700

    Complete Core Repository and Unit Of Work

---
+++ src/server/OnlineShop.Data/Infrastructure/Core/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using OnlineShop.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Data.Infrastructure.Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        OnlineShopDbContext DataContext { get; }
+
+        #region Master Data
+
+        ICoreRepository<Category> CategoryRepository { get; }
+
+        #endregion
+
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync();
+
+        ICoreRepository<T> CoreRepository<T>() where T : Entity, IEntity;
+    }
+}
---
+++ src/server/OnlineShop.Data/Infrastructure/Core/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Data.Infrastructure.Core
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly OnlineShopDbContext _dbContext;
+
+        public OnlineShopDbContext DataContext => _dbContext;
+
+        public UnitOfWork(OnlineShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        private ICoreRepository<Category> _categoryRepository;
+
+        public ICoreRepository<Category> CategoryRepository =>
+            _categoryRepository ??= new CoreRepository<Category>(_dbContext);
+
+        #region Methods
+        public int SaveChanges()
+        {
+            return _dbContext.SaveChanges();
+
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return _dbContext.SaveChangesAsync();
+        }
+       
+        public void SetDetachChanges(bool value)
+        {
+            _dbContext.ChangeTracker.AutoDetectChangesEnabled = value;
+        }
+
+        public ICoreRepository<T> CoreRepository<T>() where T : Entity, IEntity
+        {
+            return new CoreRepository<T>(_dbContext);
+        }
+
+        public void Dispose()
+        {
+            this._dbContext.Dispose();
+        }
+        #endregion
+    }
+}
